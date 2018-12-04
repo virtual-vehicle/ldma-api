74a6fc07ae0d4535ce28dea4dc0ebfce7d33af48
marijadjokicpetrovic <marija.djokic795@gmail.com>
2018-12-04 13:07:15 +0100

    added tests and documentation for authenticated API calls

--- test/aegis_api_web/controllers/event_controller_test.exs
+++ test/aegis_api_web/controllers/event_controller_test.exs
@@ -1,18 +1,16 @@
 defmodule AegisApiWeb.EventControllerTest do
   use AegisApiWeb.ConnCase
+  import AegisApi.HelperTest
 
-  test "GET /api/v1/events" do
-    conn = build_conn()
-        |> get("/api/v1/events")
-        |> doc(description: "List all events", operation_id: "list_events")
-    assert conn.status == 200
+  describe "test events" do
+    setup do
+      create_jwt_for_driver()
+    end
+
+    test "GET /api/v1/events", %{conn: conn} do
+      conn = get conn, "/api/v1/events"
+      conn |> doc(description: "List all events for existing driver", operation_id: "list_events")
+      assert conn.status == 200
+    end
   end
-
-  test "GET /api/v1/events/:id" do
-    conn = build_conn()
-        |> get("/api/v1/events/1")
-        |> doc(description: "Event with id 1", operation_id: "each_event")
-    assert conn.status == 200
-  end
-
 end
--- test/aegis_api_web/controllers/measurement_controller_test.exs
+++ test/aegis_api_web/controllers/measurement_controller_test.exs
@@ -1,18 +1,16 @@
-defmodule AegisApiWeb.MeasurementControllerTest do
-  use AegisApiWeb.ConnCase
+# defmodule AegisApiWeb.MeasurementControllerTest do
+#   use AegisApiWeb.ConnCase
+#   import AegisApi.HelperTest
 
-  # test "GET /api/v1/measurements" do
-  #   conn = build_conn()
-  #       |> get("/api/v1/measurements")
-  #       |> doc(description: "List all measurements", operation_id: "list_measurements")
-  #   assert conn.status == 200
-  # end
+#   describe "test measurements" do
+#     setup do
+#       create_jwt_for_driver()
+#     end
 
-  # TODO - we need to add id for measurments in the datbles
-  test "GET /api/v1/measurements/:trip_id" do
-    conn = build_conn()
-        |> get("/api/v1/measurements/8653e115-aebc-4ded-a785-a20e370e0120")
-        |> doc(description: "Measurement with trip_id 8653e115-aebc-4ded-a785-a20e370e0120", operation_id: "each_measurement")
-    assert conn.status == 200
-  end
-end
+#     test "GET /api/v1/measurements",  %{conn: conn} do
+#       conn =  get conn, "/api/v1/measurements"
+#       conn |> doc(description: "List all measurements for existing driver", operation_id: "list_measurements")
+#       assert conn.status == 200
+#     end
+#   end
+# end

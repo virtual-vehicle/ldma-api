8a33037a76df2a44ffe9d4cdb6471f041e9a7528
Marija Djokic Petrovic <marija.djokicpetrovic@v2c2.at>
2019-02-26 20:31:34 +0100

    Fix trips_info api

--- lib/aegis_api_web/controllers/tripsinfo_controller.ex
+++ lib/aegis_api_web/controllers/tripsinfo_controller.ex
@@ -10,12 +10,12 @@ defmodule AegisApiWeb.TripsInfoController do
   def show(conn, _params) do
     driver = Guardian.Plug.current_resource(conn)
     trips = AegisApi.Repo.one(from t in AegisApiWeb.Trip, where: t.driver_id == ^driver.driver_id, select: count(t))
-    events = AegisApi.Repo.one(from e in AegisApiWeb.Event, join: t in AegisApiWeb.Trip, on: e.trip_id == t.trip_id, where: t.driver_id == ^driver.driver_id, select: count(e))
+    events = AegisApi.Repo.one(from e in AegisApiWeb.Event, where: t.driver_id == ^driver.driver_id, select: count(e))
     distance = AegisApi.Repo.one(from t in AegisApiWeb.Trip, where: t.driver_id == ^driver.driver_id, select: sum(t.distance))
     totaltime = AegisApi.Repo.one(from t in AegisApiWeb.Trip, where: t.driver_id == ^driver.driver_id, select: sum(t.duration))
-    accelartions = AegisApi.Repo.one(from e in AegisApiWeb.Event, join: t in AegisApiWeb.Trip, on: e.trip_id == t.trip_id, where: t.driver_id == ^driver.driver_id and e.event_type == "ACCELERATION", select: count(e))
-    brakes = AegisApi.Repo.one(from e in AegisApiWeb.Event, join: t in AegisApiWeb.Trip, on: e.trip_id == t.trip_id, where: t.driver_id == ^driver.driver_id and e.event_type == "BRAKE", select: count(e))
-    standstills = AegisApi.Repo.one(from e in AegisApiWeb.Event, join: t in AegisApiWeb.Trip, on: e.trip_id == t.trip_id, where: t.driver_id == ^driver.driver_id and e.event_type == "STANDSTILL", select: count(e))
+    accelartions = AegisApi.Repo.one(from e in AegisApiWeb.Event, where: e.driver_id == ^driver.driver_id and e.event_type == "ACCELERATION", select: count(e))
+    brakes = AegisApi.Repo.one(from e in AegisApiWeb.Event, where: e.driver_id == ^driver.driver_id and e.event_type == "BRAKE", select: count(e))
+    standstills = AegisApi.Repo.one(from e in AegisApiWeb.Event, where: e.driver_id == ^driver.driver_id and e.event_type == "STANDSTILL", select: count(e))
 
     conn |> render("tripsinfo.json", trips: trips, events: events, distance: distance, totaltime: totaltime, accelartions: accelartions, brakes: brakes, standstills: standstills)
   end
